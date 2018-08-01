b6b376ca7809c27c649d3a6c167429d8f804f1ac
jaokeeffe <jaokeeffe@outlook.com>
2018-08-01 18:59:38 +1000

    First version of blockchain

---
+++ blockchain/main.js
@@ -0,0 +1,71 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block{
+    constructor(index, timestamp, data, previousHash = ''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+    calculateHash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log('Block mined: ' + this.hash);
+    }
+}
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
+    }
+    createGenesisBlock() {
+        return new Block(0, "01/01/2017", "Genesis block", "0");
+    }
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+    }
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+let jokCoin = new Blockchain();
+console.log('Mining Block 1...');
+jokCoin.addBlock(new Block(1, "16/02/1970", { age: 48 }));
+console.log('Mining block 2...');
+jokCoin.addBlock(new Block(2, "11/10/2001", { age: 16 }));
+
+// jokCoin.addBlock(new Block(1, "16/02/1970", { age: 48 }));
+// jokCoin.addBlock(new Block(2, "11/10/2001", { age: 16 }));
+// jokCoin.addBlock(new Block(3, "04/06/2003", { age: 15 }));
+// jokCoin.addBlock(new Block(4, "19/11/2007", { age: 11 }));
+
+// console.log(JSON.stringify(jokCoin, null, 4));
+// console.log('Is blockchain valid? ' + jokCoin.isChainValid());
+// jokCoin.chain[1].data = { age: 25 };
+// jokCoin.chain[1].hash = jokCoin.chain[1].calculateHash();
+// console.log('Is blockchain valid? ' + jokCoin.isChainValid());
